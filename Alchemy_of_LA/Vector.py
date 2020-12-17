8d7c562b8270045a9e3ce56950cc106332c6089d
Yuchen Ran <ml192yr@leeds.ac.uk>
2020-12-17 05:56:22 +0000

    add zero vector code

--- Alchemy_of_LA/Vector.py
+++ Alchemy_of_LA/Vector.py
@@ -4,7 +4,48 @@
 class Vector:
 
     def __init__(self, lst):
-        self._values = lst
+        self._values = list(lst)
+
+    @classmethod
+    def zero(cls, dim):
+        """返回一个dim维的零向量"""
+        return cls([0] * dim)
+
+    def __add__(self, another):
+        """向量加法，返回结果向量"""
+        assert len(self) == len(another), \
+            "Error in adding. Length of vectors must be same."
+
+        return Vector([a + b for a, b in zip(self, another)])
+        # 另外一种写法
+        # return Vector([a + b for a, b in zip(self._values, another._values)])
+
+    def __iter__(self):
+        """返回向量的迭代器"""
+        return self._values.__iter__()
+
+    def __sub__(self, another):
+        """向量减法，返回结果向量"""
+        assert len(self) == len(another), \
+            "Error in subtracting. Length of vectors must be same."
+
+        return Vector([a - b for a, b in zip(self, another)])
+
+    def __mul__(self, k):
+        """返回数量乘法的结果向量：self * k"""
+        return Vector([k * e for e in self])
+
+    def __rmul__(self, k):
+        """返回数量乘法的结果向量：k * self"""
+        return self * k
+
+    def __pos__(self):
+        """返回向量取正的结果向量"""
+        return 1 * self
+
+    def __neg__(self):
+        """返回向量取负的结果向量"""
+        return -1 * self
 
     def __getitem__(self, index):
         """取向量的第index个元素"""
